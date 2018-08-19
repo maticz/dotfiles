63bdbde3e57fb547c26dcee864308805d92bbb4b
Matic Žgur <mr.zgur@gmail.com>
2018-08-19 08:10:35 +0200

    Add install.py.

---
+++ install.py
@@ -0,0 +1,68 @@
+import os
+import pathlib
+import shutil
+import subprocess
+
+packages = [
+    'vim-X11',
+    'i3',
+    'i3status',
+    'dmenu',
+    'i3lock',
+    'dunst',
+    'xbacklight',
+    'feh',
+    'network-manager-applet',
+]
+
+def install_packages():
+    params = ['sudo', 'dnf', '-y', 'install']
+    params.extend(packages)
+    result = subprocess.run(params)
+    print(result.stdout)
+
+def create_links(root_dir):
+    if not root_dir.endswith('/'):
+        root_dir = root_dir + '/'
+
+    home = str(pathlib.Path.home()) + '/tttt'
+
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        relative_dir = dirpath.replace(root_dir, '')
+        for f in filenames:
+            src = os.path.join(dirpath, f)
+            dst_dir = os.path.join(home, relative_dir)
+            dst = os.path.join(dst_dir, f)
+
+            print('About to create a new link.')
+            print('Source: {}'.format(src))
+            print('Destination: {}'.format(dst))
+
+            os.makedirs(dst_dir, exist_ok=True)
+
+            if os.path.exists(dst):
+                if os.path.realpath(dst) == src:
+                    print('Link already exists and points to correct source, skipping.')
+                    print('----')
+                    continue
+
+                print('Destination file exists. Creating backup for it.')
+                shutil.move(dst, dst+'_bak')
+
+            os.symlink(src, dst)
+            print('Link created: {}.'.format(dst))
+            print('----')
+
+
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    script_home = os.path.join(script_dir, 'home')
+
+    print('About to install packages')
+    install_packages()
+    print('Packages installed\n\n')
+
+    print('About to create links')
+    create_links(script_home)
+    print('Links created')
+    print('Done')
